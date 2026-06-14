35a682ed7ff5ad2bff78d888eb75866ee6d73319
DevLocal <devnull@example.com>
2026-06-14 16:44:21 +0530

    Initial commit: add LearnPlaywrightBatch2x-main

---
+++ chapter_06_Operator/39_Logical_Op.js
@@ -0,0 +1,18 @@
+// && -> AND Gate
+// || -> OR Gate
+
+let a = true;
+let b = false;
+console.log(a && b); //  AND
+console.log(a || b); //  OR
+console.log(!a); // 
+
+let a2= 10 , b2=3;
+ let sum= a2 + b2;
+//l
+
+console.log(sum);;
+console.log(42=="42");
+console.log(42==="42");
+console.log(0==""); // trastivity broken 
+console.log(5!="5"); // no type coercion
